3c7bd8b52e85ef1aba8f5595ba5aead870a026e0
ferschtain <64108461+ferschtain@users.noreply.github.com>
2025-08-01 19:45:58 +0300

    Update lampa script.js

--- lampa script.js
+++ lampa script.js
@@ -1,30 +1,6 @@
 (function() {
   'use strict';
 
-  const manifest = {
-    type: 'video',
-    version: '1.0.0',
-    name: 'VIDSRC Plugin',
-    description: 'Play movies via VIDSRC embed',
-    component: 'vidsrc-component',
-    onContextMenu: function(object) {
-      return {
-        name: 'Watch via VIDSRC',
-        description: 'Open using VIDSRC player'
-      };
-    },
-    onContextLauch: function(object) {
-      Lampa.Activity.push({
-        url: '',
-        title: 'VIDSRC Player',
-        component: 'vidsrc-component',
-        search: object.title,
-        movie: object,
-        page: 1
-      });
-    }
-  };
-
   function component(object) {
     this.create = () => this.render();
 
@@ -57,10 +33,81 @@
     };
   }
 
+  function addSourceSearch(name, uri) {
+    const source = {
+      title: name,
+      search: function(params, oncomplete) {
+        if (!params.movie || !params.movie.imdb_id) {
+          oncomplete([]);
+          return;
+        }
+
+        const cards = [{
+          title: 'Play with VIDSRC',
+          img: './img/lampa.svg',
+          year: '',
+          movie: params.movie,
+          url: `https://vidsrc.to/embed/movie/${params.movie.imdb_id}`
+        }];
+
+        oncomplete([
+          {
+            title: name,
+            results: cards
+          }
+        ]);
+      },
+      onCancel: function() {},
+      params: {
+        lazy: true
+      },
+      onSelect: function(params, close) {
+        close();
+
+        const url = params.element.url;
+
+        Lampa.Player.play({
+          title: params.element.title,
+          url: url,
+          method: 'play',
+          isonline: true
+        });
+      }
+    };
+
+    Lampa.Search.addSource(source);
+  }
+
   function startPlugin() {
-    Lampa.Component.add('vidsrc-component', component);
+    window.vidsrc_component_plugin = true;
+
+    const manifest = {
+      type: 'video',
+      version: '1.0.0',
+      name: 'VIDSRC',
+      description: 'Play movies via VIDSRC',
+      component: 'vidsrc-component',
+      onContextMenu: function(object) {
+        return {
+          name: 'Watch via VIDSRC',
+          description: ''
+        };
+      },
+      onContextLauch: function(object) {
+        Lampa.Activity.push({
+          url: '',
+          title: 'VIDSRC',
+          component: 'vidsrc-component',
+          search: object.title,
+          movie: object,
+          page: 1
+        });
+      }
+    };
+
     Lampa.Manifest.pl.push(manifest);
-    window.vidsrc_component_plugin = true; // ✅ This line is essential
+    Lampa.Component.add('vidsrc-component', component);
+    addSourceSearch('VIDSRC', 'vidsrc');
   }
 
   startPlugin();
